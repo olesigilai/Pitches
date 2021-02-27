67b5ed169acdcfb64442efc63098a68aada8a36e
olesigilai <ole_sigilai@yahoo.com>
2021-02-27 23:48:02 +0300

    forms.py imports are added

---
+++ app/main/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Title')
+    category = SelectField(u'Pitch Categories', choices=[('Product', 'Product'), ('Adverts', 'Adverts'),('Interview', 'Interview')])
+    pitch = TextAreaField('Pitch')
+    submit = SubmitField('Submit')
+    
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment')
+    submit = SubmitField('Post Comments')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
