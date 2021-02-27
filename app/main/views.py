d89f00e5d40db037dd02f21a8bf98c24a4885b67
olesigilai <ole_sigilai@yahoo.com>
2021-02-28 00:30:35 +0300

    dynamic routes added and functions

--- app/main/views.py
+++ app/main/views.py
@@ -6,3 +6,25 @@ from . import main
 from ..email import mail_message
 from .forms import PitchForm,CommentForm,UpdateProfile
 
+@main.route('/pitch/', methods = ['GET','POST'])
+@login_required
+def new_pitch():
+
+    form = PitchForm()
+
+    if form.validate_on_submit():
+        category = form.category.data
+        pitch= form.pitch.data
+        title=form.title.data
+
+        # Updated pitchinstance
+        new_pitch = Pitches(title=title,category= category,pitch= pitch,user_id=current_user.id)
+
+        title='New Pitch'
+
+        new_pitch.save_pitch()
+
+        return redirect(url_for('main.index'))
+
+    return render_template('pitch.html',form= form)
+
