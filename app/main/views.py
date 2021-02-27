2618c0228d8b4602d5e3d9ff40f7eb0731554041
olesigilai <ole_sigilai@yahoo.com>
2021-02-27 23:52:23 +0300

    imports for view.py main are made

---
+++ app/main/views.py
@@ -0,0 +1,7 @@
+from flask_login import login_required, current_user
+from flask import render_template,request,redirect,url_for, abort
+from ..models import Pitches,Role,User,Comments
+from .. import db,photos
+from . import main
+from ..email import mail_message
+from .forms import PitchForm,CommentForm,UpdateProfile
