11bcb7b953e6f9bcd7fe6ac16910d69095baaf3c
olesigilai <ole_sigilai@yahoo.com>
2021-03-01 18:46:40 +0300

    sender email is added to email.py

--- app/email.py
+++ app/email.py
@@ -7,7 +7,7 @@ sender_email = 'gideonkipkirui95@gmail.com'
 
 
 def mail_message(subject,template,to,**kwargs):    
-
+    sender_email  ='gideonkipkirui95@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
