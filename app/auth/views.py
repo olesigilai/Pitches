2f52f423059f4baf18a06824e8dce66bd9d25e97
olesigilai <ole_sigilai@yahoo.com>
2021-03-01 18:47:04 +0300

    email message is imported

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from ..models import User
 from .forms import LoginForm,RegistrationForm
 from .. import db
 from flask_login import login_user,logout_user,login_required
-
+from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -29,7 +29,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        # mail_message("Welcome to Pitch Application","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Pitch Application","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
